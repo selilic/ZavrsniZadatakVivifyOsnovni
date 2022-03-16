880c2dca8b232203dec7d9f7c72b08a03137428f
selilic <ilicselena7@gmail.com>
2022-03-16 14:49:43 +0100

    Zadatak 4.

--- posts.php
+++ posts.php
@@ -1,76 +1,19 @@
-<?php
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "";
-    $dbname = "blog";
-
-    try {
-        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $e)
-    {
-        echo $e->getMessage();
-    }
+<?php 
+    require_once('db.php'); 
+    $sql_posts = "SELECT * FROM posts ORDER BY created_at DESC";
+    $posts = getDataFromServer($sql_posts, $connection);
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
-
-    <title>Vivify Blog</title>
-
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-
-    <!-- Custom styles for this template -->
-    <link href="styles/blog.css" rel="stylesheet">
-    <link href="styles/styles.css" rel="stylesheet">
-</head>
-
-<body>
-
-<?php include('header.php') ?>
-
 <?php
-    $sql = "SELECT * FROM posts ORDER BY created_at DESC";
-    $statement = $connection->prepare($sql);
-    $statement->execute();
-    $statement->setFetchMode(PDO::FETCH_ASSOC);
-    $posts = $statement->fetchAll();
+    foreach ($posts as $post) {
 ?>
 
-<main role="main" class="container">
-    <div class="row">
-        <div class="col-sm-8 blog-main">
-
-            <?php
-                foreach ($posts as $post) {
-            ?>
-
-                <div class="blog-post">
-                    <h2 class="blog-post-title"><a href="single-post.php?post_id=<?php echo($post['id']); ?>"><?php echo($post['title']); ?></a></h2>
-                    <p class="blog-post-meta"><?php echo DateTime::createFromFormat('Y-m-j', $post['created_at'])->format('M j, Y');?> by <a href="#"><?php echo($post['author']); ?></a></p>
-                    <p><?php echo($post['body']); ?></p>
-                </div>
-
-            <?php 
-                }
-            ?>
-        </div>
-
-        <?php include('sidebar.php') ?>
-
+    <div class="blog-post">
+        <h2 class="blog-post-title"><a href="single-post.php?post_id=<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a></h2>
+        <p class="blog-post-meta"><?php echo DateTime::createFromFormat('Y-m-j', $post['created_at'])->format('M j, Y');?> by <a href="#"><?php echo $post['author']; ?></a></p>
+        <p><?php echo$post['body']; ?></p>
     </div>
-</main>
 
-<?php include('footer.php') ?>
-
-</body>
-</html>
+<?php 
+    }
+?>    
